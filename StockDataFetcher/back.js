ac275c971f0deb80c6a933649afc19d2d4c176d7
rk <rushikeshkhadikar1@gmail.com>
2021-03-12 14:16:31 +0530

    MessageAPI chrome

--- StockDataFetcher/back.js
+++ StockDataFetcher/back.js
@@ -1,1 +1,15 @@
-console.log("test")
+console.log("contextScript")
+
+// Sending messages from Content Script
+const msg = 'test from content Script'
+chrome.runtime.sendMessage({ message: {name:"rushikesh",lname:"Khadikar"} }, function(response) {
+    console.log(response);
+});
+
+
+// Listening to messages in Context Script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    // Callback
+    sendResponse({ message: 'Content script has received that message' })
+})
--- StockDataFetcher/background.js
+++ StockDataFetcher/background.js
@@ -5,6 +5,12 @@ chrome.browserAction.onClicked.addListener(buttonC)
 
 function buttonC(){
     console.log("Button Clicked")
-    chrome.tabs.executeScript(null,{code:"console.log('testestset')"});
+    chrome.tabs.executeScript(null,{file:'back.js'});
 }
 
+// Listening to messages page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    // Callback for that request
+    sendResponse({ message: "Background has received that message" });
+});
