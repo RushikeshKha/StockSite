afb07a9908ac5e2ded58a5b2c621c49d95885896
rk <rushikeshkhadikar1@gmail.com>
2021-04-01 12:16:17 +0530

    code change

--- StockDataFetcher/back.js
+++ StockDataFetcher/back.js
@@ -5,9 +5,22 @@ const msg = 'test from content Script'//MESSAGE
 chrome.runtime.sendMessage({ message: {name:"rushikesh",lname:"Khadikar"} }, function(response) {
     console.log(response);
 });//THIS FUNCTION IS USED TO SEND MESSAGE TO BACKGROUND SCRIPT
+function getStockData(num){
+    var stkdata = [];
+    for(i=2;i<num;i++){
+        var stckName = document.querySelector("body > div.js-rootresizer__contents > div.layout__area--right > div > div.widgetbar-pages > div.widgetbar-pagescontent > div.widgetbar-page.active > div.widget-1quyc-Kt.widgetbar-widget.widgetbar-widget-watchlist > div.widgetbar-widgetbody > div > div.scrollable-2mu5oKC8 > div > div.listContainer-3U2Wf-wc > div > div:nth-child("+i+") > div > div > div.firstItem-EJ_LFrif.symbolName-EJ_LFrif > span > span.inner-EJ_LFrif.symbolNameText-EJ_LFrif").textContent;
+        var stckPrice = document.querySelector("body > div.js-rootresizer__contents > div.layout__area--right > div > div.widgetbar-pages > div.widgetbar-pagescontent > div.widgetbar-page.active > div.widget-1quyc-Kt.widgetbar-widget.widgetbar-widget-watchlist > div.widgetbar-widgetbody > div > div.scrollable-2mu5oKC8 > div > div.listContainer-3U2Wf-wc > div > div:nth-child("+i+") > div > div > span.cell-EJ_LFrif.last-EJ_LFrif").textContent;
+    }
+    return {stockName:stckName,stockPrice:stckPrice};}
 
+    function getStockPrice(num){
+        var a = [];
+        for(i=2;i<=num;i++){
+            a.push(getStockData(i))
+        }
+       return a}
 setInterval(() => {
-    chrome.runtime.sendMessage({ message: {name:"rushikesh",lname:"Khadikar"} }, function(response) {
+    chrome.runtime.sendMessage({ message: getStockPrice(8) }, function(response) {
         console.log(response);
     });
 }, 10000);//THIS FUNCTION IS USED TO SET INTERVALS FOR 10SEC
